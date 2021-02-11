0c80156a356251c240a0b58bdae37511a0130b7f
Mercury Fong <wtfongaa@connect.ust.hk>
2021-02-11 15:08:32 +0800

    updated patch name, flow cond, and runfile

--- run/run_highAOA_std.sh
+++ run/run_highAOA_std.sh
@@ -6,7 +6,7 @@
 #       SLURM command or statement.
 
 
-#SBATCH -J CFD_Merc_Case1_low #Slurm job name
+#SBATCH -J CFD_Merc_Case1_hi #Slurm job name
 
 # Set the maximum runtime, uncomment if you need it
 ##SBATCH -t 48:00:00 #Maximum runtime of 48 hours
@@ -17,7 +17,7 @@
 #SBATCH --mail-type=end
 
 # Choose partition (queue), for example, partition "standard / general"
-#SBATCH -p general
+#SBATCH -p standard
 
 # Use 1 nodes and 24 cores
 #SBATCH -N 1 -n 24
@@ -37,10 +37,10 @@ name="PCase1"
 #liftDir=("liftDir           (-0.20791 0.97815 0.0 );")
 #dragDir=("dragDir           (0.97815 0.20791 0.0 );")
 
-AOA=("-AOA2" "-AOA4" "-AOA6" "-AOA8" "-AOA10")
-list=("internalField  uniform (22.98599 0.80269 0.0 );" "internalField  uniform (22.94397 1.60440 0.0 );" "internalField  uniform (22.87400 2.40415 0.0 );" "internalField  uniform (22.77617 3.20098 0.0 );" "internalField  uniform (22.65058 3.99391 0.0 );")
-liftDir=("liftDir           (-0.03490 0.99939 0.0 );" "liftDir           (-0.06976 0.99756 0.0 );" "liftDir           (-0.10453 0.99452 0.0 );" "liftDir           (-0.13917 0.99027 0.0 );" "liftDir           (-0.17365 0.98481 0.0 );")
-dragDir=("dragDir           (0.99939 0.03490 0.0 );" "dragDir           (0.99756 0.06976 0.0 );" "dragDir           (0.99452 0.10453 0.0 );" "dragDir           (0.99027 0.13917 0.0 );" "dragDir           (0.98481 0.17365 0.0 );")
+AOA=("-AOA12" "-AOA14" "-AOA16" "-AOA18" "-AOA19")
+list=("internalField  uniform (22.49739 4.78197 0.0 );" "internalField  uniform (22.31680 5.56420 0.0 );" "internalField  uniform (22.10902 6.33966 0.0 );" "internalField  uniform (21.87430 7.10739 0.0 );" "internalField  uniform (21.74693 7.48807 0.0 );")
+liftDir=("liftDir           (-0.20791 0.97815 0.0 );" "liftDir           (-0.24192 0.97030 0.0 );" "liftDir           (-0.27564 0.96126 0.0 );" "liftDir           (-0.30902 0.95106 0.0 );" "liftDir           (-0.32557 0.94552 0.0 );")
+dragDir=("dragDir           (0.97815 0.20791 0.0 );" "dragDir           (0.97030 0.24192 0.0 );" "dragDir           (0.96126 0.27564 0.0 );" "dragDir           (0.95106 0.30902 0.0 );" "dragDir           (0.94552 0.32557 0.0 );")
 
 #Pauls
 # AOA=("-AOA4")
