d9ab046f68058aa802cc69c98d4abad156614578
kyfrankie <kyfrankie@gmail.com>
2021-03-28 19:59:41 +0800

    hpc3 initial commit

---
+++ aoa.sh
@@ -0,0 +1,60 @@
+AOA=("-AOA02")
+list=("internalField  uniform (22.98598902 0.80268842 0.0 );")
+liftDir=("liftDir           (-0.03489950 0.9993908 0.0 );" )
+dragDir=("dragDir           (0.9993908 0.03489950 0.0 );" )
+
+AOA=("-AOA06")
+list=("internalField  uniform (22.87400359 2.40415466 0.0 );")
+liftDir=("liftDir           (-0.10452846 0.9945219 0.0 );")
+dragDir=("dragDir           (0.9945219 0.10452846 0.0 );")
+
+AOA=("-AOA10")
+list=("internalField  uniform (22.65057832 3.99390809 0.0 );")
+liftDir=("liftDir           (-0.17364818 0.9848078 0.0 );")
+dragDir=("dragDir           (0.9848078 0.17364818 0.0 );")
+
+AOA=("-AOA14")
+list=("internalField  uniform (22.31680170 5.56420360 0.0 );")
+liftDir=("liftDir           (-0.24192190 0.9702957 0.0 );")
+dragDir=("dragDir           (0.9702957 0.24192190 0.0 );")
+
+AOA=("-AOA16")
+list=("internalField  uniform (22.10901901 6.33965918 0.0 );")
+liftDir=("liftDir           (-0.27563736 0.9612617 0.0 );")
+dragDir=("dragDir           (0.9612617 0.27563736 0.0 );")
+
+AOA=("-AOA18")
+list=("internalField  uniform (21.87429987 7.10739087 0.0 );")
+liftDir=("liftDir           (-0.30901699 0.9510565 0.0 );")
+dragDir=("dragDir           (0.9510565 0.30901699 0.0 );")
+
+----
+AOA=("-AOA04")
+list=("internalField  uniform (22.94397316 1.60439890 0.0 );")
+liftDir=("liftDir           (-0.06975647 0.9975641 0.0 );")
+dragDir=("dragDir           (0.9975641 0.06975647 0.0 );")
+
+AOA=("-AOA08")
+list=("internalField  uniform (22.77616558 3.20098132 0.0 );")
+liftDir=("liftDir           (-0.13917310 0.9902681 0.0 );")
+dragDir=("dragDir           (0.9902681 0.13917310 0.0 );")
+
+AOA=("-AOA12")
+list=("internalField  uniform (22.49739482 4.78196889 0.0 );")
+liftDir=("liftDir           (-0.20791169 0.9781476 0.0 );")
+dragDir=("dragDir           (0.9781476 0.20791169 0.0 );")
+
+AOA=("-AOA13")
+list=("internalField  uniform (22.41051149 5.17387425 0.0 );")
+liftDir=("liftDir           (-0.22495105 0.9743701 0.0 );")
+dragDir=("dragDir           (0.9743701 0.22495105 0.0 );")
+
+AOA=("-AOA15")
+list=("internalField  uniform (22.21629400 5.95283804 0.0 );")
+liftDir=("liftDir           (-0.25881905 0.9659258 0.0 );")
+dragDir=("dragDir           (0.9659258 0.25881905 0.0 );")
+
+AOA=("-AOA17")
+list=("internalField  uniform (21.99500939 6.72454921 0.0 );")
+liftDir=("liftDir           (-0.29237170 0.9563048 0.0 );")
+dragDir=("dragDir           (0.9563048 0.29237170 0.0 );")
